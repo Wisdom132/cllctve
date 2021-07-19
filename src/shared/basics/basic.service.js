d791e645822747d537e4749cc2f01045677efc5e
Ayomide <adeayo35@gmail.com>
2021-07-19 04:05:06 +0100

    feature/basic crud service to speed up dev

---
+++ src/shared/basics/basic.service.js
@@ -0,0 +1,43 @@
+const { Model } = require('mongoose');
+const { NotFoundException } = require('../exceptions');
+const { Helper } = require('../helpers/index');
+
+class BasicServiceImpl {
+  model;
+  populate = [];
+  modelName;
+
+  create(createDto) {
+    return this.model.create(createDto);
+  }
+
+  findAll(pagination, filter = {}) {
+    return Helper.paginate(this.model, pagination, filter);
+  }
+
+  list(options = {}) {
+    return this.model.find(options).populate(this.populate);
+  }
+
+  async findOne(id) {
+    const response = await this.model.findOne({ _id: id });
+    if (!response) {
+      throw NotFoundException(`${this.modelName} Not Found`);
+    }
+    return response;
+  }
+
+  async update(id, updateDto) {
+    await this.findOne(id);
+    await this.model.updateOne({ _id: id }, updateDto);
+    return this.findOne(id);
+  }
+
+  async remove(id) {
+    await this.findOne(id);
+    await this.model.deleteOne({ _id: id });
+    return {};
+  }
+}
+
+module.exports = BasicServiceImpl;
